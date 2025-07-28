97c944a770699623fb283894169425ea954e0bf4
Nithin Seshadri <seshn@mac.lan>
2025-07-28 09:19:20 -0400

    Added an additional invocation to the LLM to get the column headers from the file so that it can be included as additional context to the final llm call to generate python code. Also, added a README file.

--- util/util.py
+++ util/util.py
@@ -1,10 +1,19 @@
 import anthropic
 import os
 import re
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def invoke_claude(model: str, prompt: str, system_prompt: str, assist_content= "", stop_sequences=None, max_tokens=1024) -> str:
+
+    api_key = os.environ.get("ANTHROPIC_API_KEY")
+
+    if not api_key:
+        return "Anthropic API key not found in environment"
+    
     client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
+
     response = client.messages.create(
         model=model,
         system=system_prompt, 
